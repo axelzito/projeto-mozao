5d5747f466cc42290c2e64cde6e9d08d538f98c8
axelzito <axel.ataides@gmail.com>
2018-10-21 20:45:08 -0300

    preparando ambiente

---
+++ app/views/principals/_principal.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! principal, :id, :created_at, :updated_at
+json.url principal_url(principal, format: :json)
---
+++ db/migrate/20181021233150_create_principals.rb
@@ -0,0 +1,8 @@
+class CreatePrincipals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :principals do |t|
+
+      t.timestamps
+    end
+  end
+end
