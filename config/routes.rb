f18159fb2260c70dfc8c3fab43a03ea71453e6ef
Axel Miguez <axel.ataides@gmail.com>
2018-10-22 16:22:15 +0000

    teste video

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root 'principals#index'
 
-  resources :principals
+  resources :principals do
+    
+    collection do
+      get 'video'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
